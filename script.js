4e24da1b118b5bc43e9439ccd3a48d1903033127
tungld160589 <148056136+tungld160589@users.noreply.github.com>
2023-11-17 10:10:23 +0700

    ok

--- script.js
+++ script.js
@@ -19,3 +19,39 @@ dropBtn.addEventListener('mouseover', function () {
 dropDownContent.addEventListener('mouseleave', function () {
   dropDownContent.classList.add('hiden');
 });
+/*------------Slide show home---------------- */
+let slideIndex = 1;
+showSlides(slideIndex);
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+function showSlides(n) {
+  let i;
+  const slides = document.getElementsByClassName('mySlides');
+  const dots = document.getElementsByClassName('dot');
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none';
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(' active', '');
+  }
+  slides[slideIndex - 1].style.display = 'block';
+  dots[slideIndex - 1].className += ' active';
+}
+function nextSlide() {
+  for (let i = 0; i < slideIndex.length; i++) {
+    showSlides(i + 1);
+  }
+}
+// Autoplay (optional)
+// Switch to the next slide every 3 seconds
+setInterval(nextSlide, 1000);
