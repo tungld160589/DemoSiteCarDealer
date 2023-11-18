4d6699822e7185516292620041f6b83e9878371b
tungld160589 <148056136+tungld160589@users.noreply.github.com>
2023-11-18 23:49:46 +0700

    sưa lỗi menu dropdown
    sữa lỗi menudropdown và làm scrip cho phím next prev

--- script.js
+++ script.js
@@ -12,38 +12,52 @@ window.onscroll = function () {
 };
 
 /*------------Slide show home---------------- */
+const tesla = {
+  name: 'tesla',
+  price: 5000,
+  color: 'green',
+  img: '../img/tesla.jpg',
+};
+const bmw = {
+  name: 'bmw',
+  price: 6000,
+  color: 'blue',
+  img: '../img/bmw.jpg',
+};
+const ford = {
+  name: 'ford',
+  price: 7000,
+  color: 'yellow',
+  img: '../img/ford.jpg',
+};
+const productFeature = [tesla, bmw, ford];
 let slideIndex = 1;
-showSlides(slideIndex);
-function plusSlides(n) {
-  showSlides((slideIndex += n));
-}
-function currentSlide(n) {
-  showSlides((slideIndex = n));
-}
-function showSlides(n) {
-  let i;
-  const slides = document.getElementsByClassName('mySlides');
-  const dots = document.getElementsByClassName('dot');
-  if (n > slides.length) {
-    slideIndex = 1;
+const nextSlideTop = document.querySelector('.next-slide-top');
+const prevSlideTop = document.querySelector('.prev-slide-top');
+const imgSlideTop = document.querySelector('.img-slide-top');
+console.log(nextSlideTop);
+console.log(imgSlideTop);
+// imgSlideTop.src = productFeature[0].img;
+nextSlideTop.addEventListener('click', function () {
+  if (slideIndex == productFeature.length) {
+    imgSlideTop.src = productFeature[0].img;
+    slideIndex = 0;
   }
-  if (n < 1) {
-    slideIndex = slides.length;
+  if (slideIndex < productFeature.length) {
+    imgSlideTop.src = productFeature[slideIndex].img;
+    slideIndex++;
   }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = 'none';
+});
+prevSlideTop.addEventListener('click', function () {
+  if (slideIndex == 1) {
+    imgSlideTop.src = productFeature[productFeature.length - 1].img;
+    slideIndex = productFeature.length;
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(' active', '');
+  if (slideIndex > 1) {
+    imgSlideTop.src = productFeature[slideIndex - 1].img;
+    slideIndex--;
   }
-  slides[slideIndex - 1].style.display = 'block';
-  dots[slideIndex - 1].className += ' active';
-}
-function nextSlide() {
-  for (let i = 0; i < slideIndex.length; i++) {
-    showSlides(i + 1);
-  }
-}
+});
 // Autoplay (optional)
 // Switch to the next slide every 3 seconds
-setInterval(nextSlide, 1000);
+// setInterval(nextSlide, 1000);
